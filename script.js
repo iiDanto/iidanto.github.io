db46604aeea80bcd48439e3d6a886a42dffbd464
iiDanto <158697195+iiDanto@users.noreply.github.com>
2024-02-03 07:31:44 +0100

    Added script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Scrolling down
+        document.body.classList.add("scrolling-down");
+    } else {
+        
+        document.body.classList.remove("scrolling-down");
+    }
+
+    lastScrollTop = currentScroll;
+});
+
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+
+    if (section) {
+        window.scrollTo({
+            top: section.offsetTop - document.querySelector('nav').offsetHeight,
+            behavior: 'smooth'
+        });
+    }
+}
